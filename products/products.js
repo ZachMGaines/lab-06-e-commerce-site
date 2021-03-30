5cf9d27708da80f2b4f2d6ccddfeb9d1ce99c1df
Zachary Gaines <zacharygaines28@gmail.com>
2021-03-30 14:54:07 -0700

    added pets

---
+++ products/products.js
@@ -0,0 +1,55 @@
+export const pets = [
+    {
+        name: 'dog',
+        type: 'German Shepherd',
+        color: 'black',
+        size: 5,
+        hasFur: true,
+        image: 'assets/img/dog.png',
+        price: 6
+    },
+    {
+        name: 'cat',
+        type: 'Persian',
+        color: 'white',
+        size: 3,
+        hasFur: true,
+        image: 'assets/img/cat.jpg',
+        price: 5,
+    },
+    {
+        name: 'gecko',
+        type: 'Crested Gecko',
+        color: 'tan',
+        size: 1,
+        hasFur: false,
+        image: 'assets/img/gecko.jpg',
+        price: 3
+    },
+    {
+        name: 'owl',
+        type: 'Snowy owl',
+        color: 'white',
+        size: 3,
+        hasFur: false,
+        image: 'assets/img/owl.jpg',
+        price: 10
+    },
+    {
+        name: 'lion',
+        type: 'African lion',
+        color: 'brown',
+        size: 7,
+        hasFur: true,
+        image: 'assets/img/lion.jpg',
+        price: 10
+    }
+
+
+
+
+
+
+
+
+];
